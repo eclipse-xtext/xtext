49b237abafe53d26536d2ae54b9c6d3d55fc0b4f
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2026-03-18 19:09:40 +0100

    repo. grammar flaky test

---
+++ org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/editor/contentassist/flaky/XtextContentAssistTestSubclass8.java
@@ -0,0 +1,7 @@
+package org.eclipse.xtext.xtext.ui.editor.contentassist.flaky;
+
+import org.eclipse.xtext.xtext.ui.editor.contentassist.XtextContentAssistTest;
+
+@SuppressWarnings("restriction")
+public class XtextContentAssistTestSubclass8 extends XtextContentAssistTest {
+}
