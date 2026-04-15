825e77be49112852f7359f81f2a35452947a1224
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2026-04-15 20:09:13 +0200

    chore: bootstrap mwe 2.26.0.M1

--- org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
+++ org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
@@ -52,14 +52,14 @@ public class XtextVersion {
 	 * @noreference
 	 */
 	public String getMweVersion() {
-		return "2.25.0";
+		return "2.26.0.M1";
 	}
 
 	/**
 	 * @noreference
 	 */
 	public String getMweBuildNumber() {
-		return "";
+		return "S202604151752";
 	}
 
 	public String getAntlrGeneratorVersion() {
