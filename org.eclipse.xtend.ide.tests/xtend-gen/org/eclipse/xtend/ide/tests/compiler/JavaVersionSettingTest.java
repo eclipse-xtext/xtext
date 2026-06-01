b3efb5d57550be1e43c34a6f458e5cc70c0d77da
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2026-06-01 21:04:17 +0200

    More waits

--- org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/compiler/JavaVersionSettingTest.java
+++ org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/compiler/JavaVersionSettingTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, 2024 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2015, 2026 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -68,6 +68,7 @@ public class JavaVersionSettingTest extends Assert {
     try {
       this.workbenchTestHelper.tearDown();
       WorkbenchTestHelper.createPluginProject(WorkbenchTestHelper.TESTPROJECT_NAME, JavaVersion.JAVA8);
+      IResourcesSetupUtil.waitForJdtIndex();
       StringConcatenation _builder = new StringConcatenation();
       _builder.append("package mypackage");
       _builder.newLine();
